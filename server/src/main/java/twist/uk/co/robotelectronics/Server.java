41b394dde907bad2eec0a8b0eca887d71287fb3a
Timofey <p.u.timofey@gmail.com>
2016-04-28 23:36:14 +0300

    fix server interface

--- server/src/main/java/twist/uk/co/robotelectronics/Server.java
+++ server/src/main/java/twist/uk/co/robotelectronics/Server.java
@@ -1,8 +1,7 @@
 package twist.uk.co.robotelectronics;
 
 import java.io.Closeable;
-import java.io.IOException;
 
 public interface Server extends Closeable {
-    void listen(RequestListener listener) throws IOException;
+    void listen(RequestListener listener);
 }
